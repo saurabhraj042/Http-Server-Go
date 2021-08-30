3683e5cfd79f1a53a53a29eb1134743b9dba23a0
saurabhraj042 <saurabhraj042@gmail.com>
2021-08-30 14:35:28 +0530

    add logic to PlayerServer() to pass the new subtest

--- server.go
+++ server.go
@@ -3,9 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 // PlayerServer currently returns Hello, world given _any_ request.
+//server.go
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+    player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+    if player == "Pepper" {
+        fmt.Fprint(w, "20")
+        return
+    }
+
+    if player == "Floyd" {
+        fmt.Fprint(w, "10")
+        return
+    }
 }
