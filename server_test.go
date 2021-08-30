21a4452c18a1ff1436bb29bffd0a83794907ac34
saurabhraj042 <saurabhraj042@gmail.com>
2021-08-30 14:31:20 +0530

    refactor TestGETPlayers()

--- server_test.go
+++ server_test.go
@@ -7,18 +7,18 @@ import (
 )
 
 func TestGETPlayers(t *testing.T) {
-	request, _ := http.NewRequest(http.MethodGet, "/", nil)
-	response := httptest.NewRecorder()
-
-	PlayerServer(response, request)
-
 	t.Run("returns Pepper's score", func(t *testing.T) {
-		got := response.Body.String()
-		want := "20"
+        request, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
+        response := httptest.NewRecorder()
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
+        PlayerServer(response, request)
+
+        got := response.Body.String()
+        want := "20"
+
+        if got != want {
+            t.Errorf("got %q, want %q", got, want)
+        }
+    })
 
 }
