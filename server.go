cb46334ba1ef48ca5d2735204b78b2960908afb3
saurabhraj042 <saurabhraj042@gmail.com>
2021-08-30 15:33:17 +0530

    fix argument type in ServeHTTP()

--- server.go
+++ server.go
@@ -14,7 +14,7 @@ type PlayerServer struct {
 	store PlayerStore
 }
 
-func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r http.Request) {
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players")
 	fmt.Fprint(w, p.store.GetPlayerScore(player))
 }
