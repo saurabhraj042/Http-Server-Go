0d7a4873db5e1634bed7ff9aee95fbe53b434dc7
saurabhraj042 <saurabhraj042@gmail.com>
2021-09-10 09:23:54 +0530

    add store logic to InMemoryPlayerStore

--- main.go
+++ main.go
@@ -4,17 +4,27 @@ import (
     "log"
     "net/http"
 )
+func NewInMemoryPlayerStore() *InMemoryPlayerStore{
+	return &InMemoryPlayerStore{
+		map[string]int{},
+	}
+}
 // InMemoryPlayerStore collects data about players in memory.
-type InMemoryPlayerStore struct{}
+type InMemoryPlayerStore struct{
+	store map[string]int
+}
 
 // GetPlayerScore retrieves scores for a given player.
 func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return 123
+	return i.store[name]
 }
 
-func (i *InMemoryPlayerStore) RecordWin(name string) {}
+func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.store[name]++
+}
 
 func main() {
-	server := &PlayerServer{&InMemoryPlayerStore{}}
+	store := NewInMemoryPlayerStore()
+	server := &PlayerServer{store}
 	log.Fatal(http.ListenAndServe(":5050", server))
 }
